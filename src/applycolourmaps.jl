ab0c03b002bc8aa7d10d9a09476045a6c599b918
Peter Kovesi <pk@peterkovesi.com>
2015-12-29 21:48:49 +0800

    Fixed header on applycolourmaps.jl

--- src/applycolourmaps.jl
+++ src/applycolourmaps.jl
@@ -1,3 +1,18 @@
+#=----------------------------------------------------------------------------
+
+Copyright (c) 2015 Peter Kovesi
+pk@peterkovesi.com
+ 
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, subject to the following conditions:
+ 
+The above copyright notice and this permission notice shall be included in 
+all copies or substantial portions of the Software.
+
+The Software is provided "as is", without warranty of any kind.
+
+----------------------------------------------------------------------------=#
 
 import ColorTypes, Images
 
